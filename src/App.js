5c1219fe9da539223203718d2f5eb8a2273010d3
SALIMONU AYODELE KABIR <39867606+ayodelesalimon@users.noreply.github.com>
2020-01-06 04:47:20 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import ContactPage from './components/ContactPage';
+import TabPanel from './components/PrivacyPage';
+import Contact from './components/Contact';
+
 
 function App() {
   return (
-    <ContactPage/>
+   
+    <TabPanel/>
   );
 }
 
